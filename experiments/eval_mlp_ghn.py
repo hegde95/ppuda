181d47660fb57335fd82500c4f2953260378e31b
hegde95 <hegdeshashank@yahoo.com>
2022-04-30 04:25:19 -0700

    added eval script

--- experiments/eval_mlp_ghn.py
+++ experiments/eval_mlp_ghn.py
@@ -22,82 +22,110 @@ Examples:
 
 
 import torch
-import torchvision
+# import torchvision
 import numpy as np
 from ppuda.config import init_config
-from ppuda.ghn.nn import GHN
-from ppuda.vision.loader import image_loader
-from ppuda.deepnets1m.loader import DeepNets1M
-from ppuda.deepnets1m.net import Network
-from ppuda.utils import infer, adjust_net, capacity, AvgrageMeter
-
+from ppuda.ghn.nn import MLP_GHN
+# from ppuda.vision.loader import image_loader
+from ppuda.deepnets1m.loader import MLPNets1M
+from ppuda.deepnets1m.net import MlpNetwork
+# from ppuda.utils import infer, adjust_net, capacity, AvgrageMeter
+import gym
 
 args = init_config(mode='eval')
 
 assert args.ckpt is not None, 'please specify the checkpoint for evaluation'
 ghn_device = 'cpu'  # use CPU to save GPU memory (useful to avoid OOM when evaluating large models)
-ghn = GHN.load(args.ckpt, debug_level=args.debug, device=ghn_device, verbose=True)
+ghn = MLP_GHN.load(args.ckpt, debug_level=args.debug, device=ghn_device, verbose=True)
 virtual_edges = 50 if ghn.ve else 1  # default values
 
-get_loader = lambda test_batch_size: image_loader(args.dataset,
-                                                  args.data_dir,
-                                                  test=args.split != 'val',  # model selection/tuning is based on validation images and architectures
-                                                  test_batch_size=test_batch_size,
-                                                  num_workers=args.num_workers,
-                                                  noise=args.noise,
-                                                  seed=args.seed)[1:]
+# get_loader = lambda test_batch_size: image_loader(args.dataset,
+#                                                   args.data_dir,
+#                                                   test=args.split != 'val',  # model selection/tuning is based on validation images and architectures
+#                                                   test_batch_size=test_batch_size,
+#                                                   num_workers=args.num_workers,
+#                                                   noise=args.noise,
+#                                                   seed=args.seed)[1:]
 
-images_val, num_classes = get_loader(args.test_batch_size)
-assert ghn.num_classes == num_classes, ('the evaluation image dataset and the image dataset that the GHN was trained on do not match')
+# images_val, num_classes = get_loader(args.test_batch_size)
+# assert ghn.num_classes == num_classes, ('the evaluation image dataset and the image dataset that the GHN was trained on do not match')
 
-is_imagenet = args.dataset == 'imagenet'
+# is_imagenet = args.dataset == 'imagenet'
 
-graphs_queue = DeepNets1M.loader(split=args.split,
+# graphs_queue = DeepNets1M.loader(split=args.split,
+#                                  nets_dir=args.data_dir,
+#                                  large_images=is_imagenet,
+#                                  virtual_edges=virtual_edges,
+#                                  arch=args.arch)
+graphs_queue = MLPNets1M.loader(1,
+                                    split='train',
                                  nets_dir=args.data_dir,
-                                 large_images=is_imagenet,
+                                 large_images=False,
                                  virtual_edges=virtual_edges,
                                  arch=args.arch)
 
-top1_all, top5_all = AvgrageMeter('se'), AvgrageMeter('se')
+# top1_all, top5_all = AvgrageMeter('se'), AvgrageMeter('se')
 
-for graphs in graphs_queue:
+# for graphs in graphs_queue:
 
-    assert len(graphs) == 1, ('only one architecture per batch is supported in the evaluation mode', len(graphs))
-    net_args, net_idx = graphs.net_args[0], graphs.net_inds[0]
+#     assert len(graphs) == 1, ('only one architecture per batch is supported in the evaluation mode', len(graphs))
+#     net_args, net_idx = graphs.net_args[0], graphs.net_inds[0]
 
-    if isinstance(net_args['genotype'], str):
-        model = adjust_net(eval('torchvision.models.%s(num_classes=%d)' % (net_args['genotype'], num_classes)), is_imagenet).eval()
-    else:
-        model = Network(num_classes=num_classes,
-                        is_imagenet_input=is_imagenet,
-                        **net_args).eval()
-    print('\nEvaluation of arch={} with {} parameters'.format(net_idx, capacity(model)[1]))
+#     if isinstance(net_args['genotype'], str):
+#         model = adjust_net(eval('torchvision.models.%s(num_classes=%d)' % (net_args['genotype'], num_classes)), is_imagenet).eval()
+#     else:
+#         model = Network(num_classes=num_classes,
+#                         is_imagenet_input=is_imagenet,
+#                         **net_args).eval()
+#     print('\nEvaluation of arch={} with {} parameters'.format(net_idx, capacity(model)[1]))
 
-    ghn(model, graphs.to_device(ghn_device))  # predict all the parameters
+#     ghn(model, graphs.to_device(ghn_device))  # predict all the parameters
 
-    for test_batch_size in np.round(np.geomspace(args.test_batch_size, 2, int(np.log2(args.test_batch_size))), 1):
-        try:
+#     for test_batch_size in np.round(np.geomspace(args.test_batch_size, 2, int(np.log2(args.test_batch_size))), 1):
+#         try:
 
-            if test_batch_size != images_val.batch_size:
-                images_val = get_loader(int(test_batch_size))[0]
-                print('setting the eval batch size to %d ' % (images_val.batch_size))
-            elif args.dataset == 'imagenet':
-                images_val.sampler.generator.manual_seed(args.seed)  # set the generator seed to reproduce results
+#             if test_batch_size != images_val.batch_size:
+#                 images_val = get_loader(int(test_batch_size))[0]
+#                 print('setting the eval batch size to %d ' % (images_val.batch_size))
+#             elif args.dataset == 'imagenet':
+#                 images_val.sampler.generator.manual_seed(args.seed)  # set the generator seed to reproduce results
 
-            top1, top5 = infer(model.to(args.device), images_val, verbose=True)
+#             top1, top5 = infer(model.to(args.device), images_val, verbose=True)
 
-            top1_all.update(top1, 1)
-            top5_all.update(top5, 1)
+#             top1_all.update(top1, 1)
+#             top5_all.update(top5, 1)
 
-            break
+#             break
 
-        except RuntimeError as e:
-            if str(e).find('CUDA out of memory') >= 0:
-                print('CUDA out of memory, attempting to continue with smaller batch size')
-                model.to('cpu')  # attempts to release GPU memory
-                torch.cuda.empty_cache()
-            else:
-                raise
+#         except RuntimeError as e:
+#             if str(e).find('CUDA out of memory') >= 0:
+#                 print('CUDA out of memory, attempting to continue with smaller batch size')
+#                 model.to('cpu')  # attempts to release GPU memory
+#                 torch.cuda.empty_cache()
+#             else:
+#                 raise
 
-print(u'\ndone for {} nets: (avg\u00B1standard error) top1={:.1f}\u00B1{:.1f}, top5={:.1f}\u00B1{:.1f}'.
-    format(top1_all.cnt, top1_all.avg, top1_all.dispersion, top5_all.avg, top5_all.dispersion))
+# print(u'\ndone for {} nets: (avg\u00B1standard error) top1={:.1f}\u00B1{:.1f}, top5={:.1f}\u00B1{:.1f}'.
+#     format(top1_all.cnt, top1_all.avg, top1_all.dispersion, top5_all.avg, top5_all.dispersion))
+
+
+graphs = next(graphs_queue)
+model = MlpNetwork(**graphs[0].net_args)
+# ghn(model, graphs.to_device(ghn_device))
+
+env = gym.make('HalfCheetah-v2')
+
+# eval over 10 episodes
+for i in range(10):
+    obs = env.reset()
+    done = False
+    total_reward = 0
+    while not done:
+        obs = torch.from_numpy(obs).float().to(ghn_device)
+        action = model(obs)
+        action = action.detach().cpu().numpy()
+        obs, reward, done, _ = env.step(action)
+        total_reward += reward
+        # env.render()
+    
+    print(f'episode {i} reward {total_reward}')
